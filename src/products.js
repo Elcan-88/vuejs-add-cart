26ad7356127dae639fcf2ba9cde38feac4134c9e
Elcan-88 <elcan.hemidov.1999@mail.ru>
2021-10-22 11:42:24 +0400

    Vuejs Card

---
+++ src/products.js
@@ -0,0 +1,44 @@
+export default [
+    {
+        name: "Apple",
+        id: 1,
+        img: "assets/apple.jpeg",
+        price: 4,
+        quantity: 5
+    },
+    {
+        name: "Orange",
+        id: 2,
+        img: "assets/apple.jpeg",
+        price: 3,
+        quantity: 5
+    },
+    {
+        name: "Grapes",
+        id: 3,
+        img: "assets/apple.jpeg",
+        price: 2,
+        quantity: 5
+    },
+    {
+        name: "Banana",
+        id: 4,
+        img: "assets/apple.jpeg",
+        price: 1,
+        quantity: 5
+    },
+    {
+        name: "Pineapple",
+        id: 5,
+        img: "assets/apple.jpeg",
+        price: 4,
+        quantity: 5
+    },
+    {
+        name: "Guava",
+        id: 6,
+        img: "assets/apple.jpeg",
+        price: 3,
+        quantity: 5
+    }
+]
